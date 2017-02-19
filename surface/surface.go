333b4df6648b73c9bbd33ff8863cd21b059e3f8f
Radoslaw Kowalski <radek.kwlsk@gmail.com>
2017-02-20 00:35:33 +0100

    polygons colored according to `z` value, higher the `z` more red the color, lower the `z` more blue color

--- surface/surface.go
+++ surface/surface.go
@@ -18,7 +18,7 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.5' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
@@ -26,11 +26,18 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
+			_, _, z := getXYZ(i, j)
+			z *= 10
+			alpha := math.Min(1.0, math.Abs(z))
+			color := uint32(0xff * alpha)
+			if z > 0 {
+				color <<= 16
+			}
 			if anyIsNan([]float64{ax, ay, bx, by, cx, cy, dx, dy}) {
 				continue
 			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill: #%06x'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
 		}
 	}
 	fmt.Println("</svg>")
@@ -44,15 +51,19 @@ func anyIsNan(floats []float64) bool {
 	return false
 }
 func corner(i, j int) (float64, float64) {
-	x := xyrange * (float64(i)/cells - .5)
-	y := xyrange * (float64(j)/cells - .5)
-
-	z := f(x, y)
+	x, y, z := getXYZ(i, j)
 
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 
 	return sx, sy
+}
+func getXYZ(i, j int) (float64, float64, float64) {
+	x := xyrange * (float64(i)/cells - .5)
+	y := xyrange * (float64(j)/cells - .5)
+	z := f(x, y)
+
+	return x, y, z
 }
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y)
